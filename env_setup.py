6364dd9397372a0315706fbe214c65befee59346
yuliya-sakharava <yuliya_sakharava@outlook.com>
2023-11-19 19:34:05 +0400

    Secrets encrypted

--- env_setup.py
+++ env_setup.py
@@ -1,18 +1,24 @@
 from dataclasses import dataclass
 from dotenv import load_dotenv
 import os
+import json
 
 
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 PATH_SINGLE_EMPLOYEE = os.path.join(ROOT_PATH, "schemas", "single_employee.json")
+SECRET_FILE = os.path.join(ROOT_PATH, "secrets", "secrets.json")
 # PATH_DATASET = os.path.join(ROOT_PATH, "database", "dataset.json")
+
+# deserialization
+with open(SECRET_FILE, 'r') as f:
+    secrets = json.load(f)
 
 
 @dataclass
 class Credentials:
     load_dotenv()
-    APP_USERNAME: str = os.getenv("APP_USERNAME")
-    APP_PASSWORD: str = os.getenv("APP_PASSWORD")
+    APP_USERNAME: str = secrets['username']
+    APP_PASSWORD: str = secrets['password']
 
     @classmethod
     def get_env_variables(cls):
@@ -27,3 +33,6 @@ class Endpoints:
     BASE_URL: str = "https://employees-api-i9ae.onrender.com"
     TOKEN_URL: str = f"{BASE_URL}/generate-token"
     EMPLOYEES_URL: str = f"{BASE_URL}/employees"
+
+
+
